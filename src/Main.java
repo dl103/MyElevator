2a5bd515f0fd5369fb7f7fc689dd2b4fd905183b
David Le <dl103@duke.edu>
2014-03-25 01:46:50 -0400

    Fixed test cases

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,6 @@
 public class Main {
 
 	public static void main (String args[]) {
-		args = new String[3];
-		args[0]="1";
 		if (args[0].equals("0")) {
 			TestCase tc = new TestCase();
 			tc.go();
